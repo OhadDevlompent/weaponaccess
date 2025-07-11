2220533ed7219f6d9045fec9c8ea617a2fd742d6
Ohad <105451510+OhadDevlompent@users.noreply.github.com>
2025-07-11 18:36:39 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,8 @@ client_script '@ElectronAC/src/include/client.lua'
 fx_version 'cerulean'
 game 'gta5'
 
-description 'Open-source weapon access restriction (QBCore, ESX, old ESX)'
-author 'ChatGPT (o3)'
+description 'weapon access restriction'
+author '.Ohad'
 
 client_scripts {
     'config.lua',
