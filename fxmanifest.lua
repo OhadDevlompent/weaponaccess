e78dfd613e7436bcef0980542a22a83f4b185da0
Ohad <105451510+OhadDevlompent@users.noreply.github.com>
2025-07-11 16:58:55 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+client_script '@ElectronAC/src/include/client.lua'
+
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Open-source weapon access restriction (QBCore, ESX, old ESX)'
+author 'ChatGPT (o3)'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
